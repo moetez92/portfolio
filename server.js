53d17e3d0e2140fdc9d7163dea4ada5cc74ecd48
unknown <you@example.com>
2021-02-11 00:15:59 +0100

    fix port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const connectDB = require("./config/db");
-
+const port=process.env.PORT || 5000
 connectDB();
 
 app.use(express.json({extended:false}))
 app.use("/api/user",require("./routes/user"));
 app.use("/api/project",require("./routes/project"));
 app.use("/api/auth",require("./routes/auth"));
-app.listen(4000,()=>console.log("server is running correctly"));
+app.listen(port,()=>console.log("server is running correctly"));
